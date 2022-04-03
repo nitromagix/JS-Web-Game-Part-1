e1051f58f0aa7b311421405d53d01a0c433147d1
Martin <nitromagix@gmail.com>
2022-04-03 09:13:16 -0700

    refactored crazy background rendering function

--- index.js
+++ index.js
@@ -1,85 +1,74 @@
-
 const IMAGE_DIR = 'assets/'
 const DEFAULT_BG_IMAGE_WIDTH = 100;
 const DEFAULT_BG_IMAGE_HEIGHT = 100;
-const GRASS_STOP_SKY_START_POS = 500
+const HORIZON = 525
 
-function createImageElement(source) {
+function addImage(imageName, left, bottom) {
+
    let image = document.createElement('img')
-   image.src = source;
-   return image;
+   image.src = IMAGE_DIR + imageName;
+   image.style.position = 'fixed';
+   image.style.left = left;
+   image.style.bottom = bottom;
+   document.body.append(image);
+   return image
 }
 
-function addFixedElementToBody(element, left, bottom) {
-   element.style.position = 'fixed';
-   element.style.left = left;
-   element.style.bottom = bottom;
-   document.body.append(element);
-   return element;
-}
-
-function newInteractiveItem(element) {
-   element.addEventListener('dblclick', function(e) {
+function makeElementRemovable(element) {
+   element.addEventListener('dblclick', function (e) {
       element.remove();
    });
    return element;
 }
 
-   
+let getClient = function () {
+   return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight
+   }
+}
+
 // Attaching the event listener function to window's resize event
 // window.addEventListener("resize", renderBackground);
 
 function renderBackground() {
-   let clientWidth = document.documentElement.clientWidth;
-   let clientHeight = document.documentElement.clientHeight;
+   let client = getClient();
 
-   let heightMultiplier = parseInt(clientHeight / DEFAULT_BG_IMAGE_HEIGHT);
-   let heightMod = GRASS_STOP_SKY_START_POS % DEFAULT_BG_IMAGE_HEIGHT;
-
-   let bgImageWidth = DEFAULT_BG_IMAGE_WIDTH;
-   let i = 0;
-   for (i; i <= heightMultiplier; i++) {
-      let bgImageHeight = ((i === heightMultiplier) && (heightMod > 0)) ? heightMod : DEFAULT_BG_IMAGE_HEIGHT;
-      ypos = i * bgImageHeight;
-      let bgSource = ypos >= GRASS_STOP_SKY_START_POS ? 'sky.png' : 'grass.png';
-      var xpos = 0
-      while (xpos  < clientWidth) {
-         let bg = createImageElement(IMAGE_DIR + bgSource);
-         bg.height = bgImageHeight;
-         addFixedElementToBody(bg, xpos + 'px', ypos + 'px');
-         xpos = xpos + bgImageWidth;
+   let ypos = 0;
+   while (ypos < client.height) {
+      let bgImageName = ypos < HORIZON ? 'grass.png' : 'sky.png';
+      var xpos = 0;
+      while (xpos < client.width) {
+         addImage(bgImageName, xpos + 'px', ypos + 'px');
+         xpos = xpos + DEFAULT_BG_IMAGE_WIDTH;
       }
+      ypos = ypos + DEFAULT_BG_IMAGE_HEIGHT;
    }
 }
 
 
 renderBackground();
 
-let greenGuy = createImageElement(IMAGE_DIR + 'green-character.gif');
-addFixedElementToBody(greenGuy, '100px', '100px');
+let images = [
+   ['green-character.gif', '100px', '100px'],
+   ['pine-tree.png', '450px', '200px'],
+   ['tree.png', '200px', '300px'],
+   ['pillar.png', '350px', '100px'],
+   ['crate.png', '150px', '200px'],
+   ['well.png', '500px', '425px']
+];
 
-let pineTree = createImageElement(IMAGE_DIR + 'pine-tree.png');
-addFixedElementToBody(pineTree, '450px', '200px');
+for (i in images) {
+   addImage(images[i][0], images[i][1], images[i][2])
+}
 
-let tree = createImageElement(IMAGE_DIR + 'tree.png');
-addFixedElementToBody(tree, '200px', '300px');
+let items = [
+   ['sword.png', '500px', '405px'],
+   ['sheild.png', '165px', '185px'],
+   ['staff.png', '600px', '100px']
+]
 
-let pillar = createImageElement(IMAGE_DIR + 'pillar.png');
-addFixedElementToBody(pillar, '350px', '100px');
-
-let crate = createImageElement(IMAGE_DIR + 'crate.png');
-addFixedElementToBody(crate, '150px', '200px');
-
-let well = createImageElement(IMAGE_DIR + 'well.png');
-addFixedElementToBody(well, '500px', '425px');
-
-let sword = createImageElement(IMAGE_DIR + 'sword.png');
-addFixedElementToBody(newInteractiveItem(sword), '500px', '405px');
-
-let shield = createImageElement(IMAGE_DIR + 'sheild.png');
-addFixedElementToBody(newInteractiveItem(shield), '165px', '185px');
-
-let staff = createImageElement(IMAGE_DIR + 'staff.png');
-addFixedElementToBody(newInteractiveItem(staff), '600px', '100px');
-
-
+for (i in items) {
+   let item = addImage(items[i][0], items[i][1], items[i][2])
+   makeElementRemovable(item)
+}
